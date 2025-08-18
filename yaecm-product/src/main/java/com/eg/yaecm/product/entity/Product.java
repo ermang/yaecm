732981fb829a9caaf8f8387fb02ff3283932fb6e
ermang <ermangonul17@gmail.com>
2025-08-18 22:38:43 +0300

    init

---
+++ yaecm-product/src/main/java/com/eg/yaecm/product/entity/Product.java
@@ -0,0 +1,27 @@
+package com.eg.yaecm.product.entity;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Product extends BaseEntity {
+
+    private String name;
+
+    private String descr;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescr() {
+        return descr;
+    }
+
+    public void setDescr(String descr) {
+        this.descr = descr;
+    }
+}
